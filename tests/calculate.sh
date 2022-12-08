2920709b3c7b0ac200b54e287c7e4653dba6780d
pruzanov <pruzanov@oicr.on.ca>
2022-12-08 15:44:25 -0500

    New design of RTs

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,5 +6,5 @@ set -o pipefail
 #enter the workflow's final output directory ($1)
 cd $1
 
-#find all files, return their md5sums to std out
-for v in *.vcf;do cat $v | md5sum;done | sort -V
+#find all vcf files, return their PASS call ids to std out
+for v in *.vcf;do cat $v | grep PASS | awk '{OFS="_";print $1,$2,$4,$5}';done | sort -V
--- tests/compare.sh
+++ tests/compare.sh
@@ -4,4 +4,15 @@ set -o errexit
 set -o pipefail
 
 #actual output metrics is $1, expected output metrics is $2
-diff -bw $1 $2 
+MATCHED=$(join --nocheck-order $1 $2 | wc -l)
+NOMATCH1=$(join --nocheck-order -v 1 $1 $2 | wc -l)
+NOMATCH2=$(join --nocheck-order -v 2 $1 $2 | wc -l)
+ALL=$(( $NOMATCH1 + $NOMATCH2 + $MATCHED))
+MATCH_RATIO=$(( $MATCHED * 100 / $ALL ))
+echo "$MATCH_RATIO% variants Match"
+if [[ $MATCH_RATIO>=90 ]];then
+  exit 0
+else
+  exit 1
+fi
+~
