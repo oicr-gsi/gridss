dab9fbff6ad0977e8d276ae23a4c13166cae0abc
pruzanov <pruzanov@oicr.on.ca>
2022-12-08 22:25:13 -0500

    Another modification to compare script

--- tests/compare.sh
+++ tests/compare.sh
@@ -9,11 +9,10 @@ NOMATCH1=$(join --nocheck-order -v 1 $1 $2 | wc -l)
 NOMATCH2=$(join --nocheck-order -v 2 $1 $2 | wc -l)
 ALL=$(( $NOMATCH1 + $NOMATCH2 + $MATCHED))
 MATCH_RATIO=$(( $MATCHED * 100 / $ALL ))
-if [[ $MATCH_RATIO>=90 ]];then
+if [[ $MATCH_RATIO > 90 ]]; then
   exit 0
 else
   echo "Only $MATCH_RATIO% variants match, FAILED"
 fi
 
-exit 1
 ~
